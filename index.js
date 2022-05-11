fc736d12e9736f70ce40467d5c8dfc88944d8241
Milica Baštrica <milicabastrica@Milicas-MacBook-Pro.local>
2022-05-11 16:19:30 +0200

    Anonymous functions

--- index.js
+++ index.js
@@ -11,5 +11,16 @@ function fun(){
 }
 
 var z= "outside of function"
-console.log(w); // outside of function(local) scope
-fun()
+//console.log(w); // outside of function(local) scope
+//fun()
+
+// anonymous function
+
+// 1
+arr = [1,2,3,4,5]
+console.log(arr.map((el) => el*el));
+// 2
+((num1,num2)=> {console.log(num1+num2)} )(2,5);
+// 3
+const timesTwo = (number) => number*2;
+console.log(timesTwo(20));
