da369a2078dad2f4df0a36bca7e3cbd9b85df7b6
Milica Baštrica <milicabastrica@Milicas-MacBook-Pro.local>
2022-05-12 08:57:19 +0200

    arrays

--- index.js
+++ index.js
@@ -24,3 +24,63 @@ console.log(arr.map((el) => el*el));
 // 3
 const timesTwo = (number) => number*2;
 console.log(timesTwo(20));
+
+// arrays
+colours = ['red','orange','white','black','blue','green','yellow','purple'];
+// loop via for
+for(let i=0;i<colours.length;i++){
+    console.log(colours[i]);
+}
+// loop via foreach
+colours.forEach(colour => {
+   console.log(colour); 
+});
+
+// 1
+const arrayRotate = (arr,step) =>{
+    newArr = [];
+    for(let i=arr.length - step;i<arr.length;i++){
+        newArr.push(arr[i])
+    }
+    for(let i=0;i<arr.length-step;i++){
+        newArr.push(arr[i])   
+    }
+    return newArr;
+}
+console.log(arrayRotate([1,2,3,4,5],2));
+
+// 2
+const sum = (arr) => {
+   return arr.reduce((sum,curr) => sum+curr,50);
+}
+console.log('Sum',sum([1,2,3,4,5]));
+// 3
+
+function numToArray(num) {
+    let arr = [];
+    for(let i=0;i<=num;i++){
+        arr.push(i);
+    }
+    for(let i=num-1;i>=0;i--){
+        arr.push(i);
+    }
+    return arr;
+}
+console.log(numToArray(5));
+
+//4
+var myZoo = [
+    ["King Kong", ["gorilla", 42]],
+    ["Nemo", ["fish", 5]],
+    ["Punxsutawney Phil", ["groundhog", 11]]
+];
+
+function zooInventory(arr) {
+    let newArr = [];
+    for(let i=0; i<arr.length; i++){
+        newArr.push(`${arr[i][0]} the ${arr[i][1][0]} is ${arr[i][1][1]}`)
+    }
+    return newArr;
+}
+
+console.log(zooInventory(myZoo));
